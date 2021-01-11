fb1d7d868329559f11e81280d5f53c01da1fbe70
will <wenli.liu@inceptioniai.org>
2021-01-11 11:52:38 +0400

    element UI import

--- src/main.js
+++ src/main.js
@@ -1,3 +1,82 @@
+import {
+    // Pagination,
+    // Dialog,
+    // Autocomplete,
+    // Dropdown,
+    // DropdownMenu,
+    // DropdownItem,
+    // Menu,
+    // Submenu,
+    // MenuItem,
+    // MenuItemGroup,
+    // Input,
+    // InputNumber,
+    // Radio,
+    // RadioGroup,
+    // RadioButton,
+    // Checkbox,
+    // CheckboxButton,
+    // CheckboxGroup,
+    // Switch,
+    // Select,
+    // Option,
+    // OptionGroup,
+    Button,
+    // ButtonGroup,
+    // Table,
+    // TableColumn,
+    // DatePicker,
+    // TimeSelect,
+    // TimePicker,
+    // Popover,
+    // Tooltip,
+    // Breadcrumb,
+    // BreadcrumbItem,
+    // Form,
+    // FormItem,
+    // Tabs,
+    // TabPane,
+    // Tag,
+    // Tree,
+    // Alert,
+    // Slider,
+    // Icon,
+    // Row,
+    // Col,
+    // Upload,
+    // Progress,
+    // Spinner,
+    // Badge,
+    // Card,
+    // Rate,
+    // Steps,
+    // Step,
+    // Carousel,
+    // CarouselItem,
+    // Collapse,
+    // CollapseItem,
+    // Cascader,
+    // ColorPicker,
+    // Transfer,
+    // Container,
+    // Header,
+    // Aside,
+    // Main,
+    // Footer,
+    // Timeline,
+    // TimelineItem,
+    // Link,
+    // Divider,
+    // Image,
+    // Calendar,
+    // Backtop,
+    // PageHeader,
+    // CascaderPanel,
+    // Loading,
+    // MessageBox,
+    // Message,
+    // Notification
+} from 'element-ui';
 import Vue from 'vue';
 import axios from 'axios';
 import VueI18n from 'vue-i18n';
@@ -15,10 +94,17 @@ import en from './i18n/en';
 
 Vue.config.productionTip = false;
 Vue.prototype.$axios = axios;
-Vue.prototype.$axios.defaults.timeout = 1000 * 5;
+Vue.prototype.$axios.defaults.timeout = 1000 * 20;
 Vue.prototype.$consts = consts;
 Vue.prototype.$apis = apis;
 
+const ThirdPartyComponents = [
+    Button,
+];
+
+ThirdPartyComponents.forEach((item) => {
+    Vue.use(item);
+});
 Vue.use(VueI18n);
 Vue.use(methods);
 
