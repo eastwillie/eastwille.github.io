2b03ead1b4e4180b537f5f29a95fe16e9619d7d3
Lee Hansel Solevilla <sshanzel@yahoo.com>
2021-01-12 14:05:31 +0400

    feat: hidden overflow-x

--- src/App.vue
+++ src/App.vue
@@ -35,6 +35,7 @@ export default {
     font-size: 0.2rem;
     font-family: Lato,Helvetica Neue,Helvetica,-apple-system,BlinkMacSystemFont,Segoe UI,PingFang SC,Hiragino Sans GB,Microsoft YaHei,Arial,'Montserrat',sans-serif,Apple Color Emoji,Segoe UI Emoji,Segoe UI Symbol;
     -webkit-font-smoothing: antialiased;
+    overflow-x: hidden;
   }
 
   #view-container {
