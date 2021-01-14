b7a02142381792b9dfd94a2701462973f301b701
Lee Hansel Solevilla <sshanzel@yahoo.com>
2021-01-14 12:07:12 +0400

    feat: Request Demo Component

--- src/views/components/AppFooter/Index.vue
+++ src/views/components/AppFooter/Index.vue
@@ -1,15 +1,41 @@
 <template>
   <footer id="app-footer">
-    This is the Footer
+    <div class="try-rodin">
+      <div class="try-rodin-content">
+        <span class="try-rodin-heading">Try Rodin</span>
+        <span class="try-rodin-description">Try Rodin</span>
+      </div>
+      <request-demo
+        background-color="#1B134C"
+        border-gradient="radial-gradient(circle at top left, #3B72FF 42.58%, #6C23F5 109.64%)"
+      />
+    </div>
   </footer>
 </template>
 
+<script>
+import RequestDemo from '@/views/components/RequestDemo/Index.vue';
+
+export default {
+  components: {
+    RequestDemo,
+  },
+};
+</script>
+
 <style lang="scss" scoped>
-  #app-footer {
+#app-footer {
+  display: flex;
+  flex-direction: column;
+  padding: 0.2rem 0.86rem;
+  border-top: 1px solid #D3D3D3;
+  background: #1B134C;
+  color: white;
+
+  .try-rodin {
     display: flex;
-    justify-content: center;
-    height: 1.2rem;
-    padding: 0.16rem;
-    border-top: 1px solid #D3D3D3;
+    flex-direction: row;
+    justify-content: space-between;
   }
+}
 </style>
