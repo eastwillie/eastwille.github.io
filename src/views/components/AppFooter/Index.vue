c411b3eeff53cd3fe2cb911ff0d9aa4b4f40ccee
Lee Hansel Solevilla <sshanzel@yahoo.com>
2021-01-14 15:18:14 +0400

    feat: Try Rodin

--- src/views/components/AppFooter/Index.vue
+++ src/views/components/AppFooter/Index.vue
@@ -1,9 +1,17 @@
 <template>
-  <footer id="app-footer">
+  <footer
+    id="app-footer"
+    class="container"
+  >
     <div class="try-rodin">
       <div class="try-rodin-content">
-        <span class="try-rodin-heading">Try Rodin</span>
-        <span class="try-rodin-description">Try Rodin</span>
+        <h2 class="font-800">
+          Try Rodin
+        </h2>
+        <br>
+        <p class="font-500">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+        </p>
       </div>
       <request-demo
         background-color="#1B134C"
@@ -27,15 +35,22 @@ export default {
 #app-footer {
   display: flex;
   flex-direction: column;
-  padding: 0.2rem 0.86rem;
   border-top: 1px solid #D3D3D3;
   background: #1B134C;
   color: white;
+  padding-top: 0.2rem;
+  padding-bottom: 0.12rem;
 
   .try-rodin {
     display: flex;
     flex-direction: row;
     justify-content: space-between;
+    padding-bottom: 0.12rem;
+    border-bottom: 1px solid #FFFFFF20;
+
+    .try-rodin-content {
+      width: 42%;
+    }
   }
 }
 </style>
