218b861f085451d5af6c91378325c5204fc5179c
Lee Hansel Solevilla <sshanzel@yahoo.com>
2021-01-18 16:59:57 +0400

    feat: Environment Variables

--- src/libs/constants.js
+++ src/libs/constants.js
@@ -1,10 +1,18 @@
-// simple global variables store
-const isDevelopment = process.env.NODE_ENV === 'development';
+const {
+  NODE_ENV,
+  BASE_URL,
+  DEV_ID,
+  DEV_KEY,
+} = process.env;
+
+const isDevelopment = NODE_ENV === 'development';
 const API = isDevelopment ? '/dev' : '';
-const publicPath = process.env.BASE_URL;
+const publicPath = BASE_URL;
 
 export default {
   isDevelopment,
   API,
   publicPath,
+  DEV_ID,
+  DEV_KEY,
 };
