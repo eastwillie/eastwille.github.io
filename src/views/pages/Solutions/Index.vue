a79df3ecccf2375ba4199490e883beea4fd43372
ehovhannisyan <74859989+ehovhannisyan@users.noreply.github.com>
2021-01-18 14:24:25 +0400

    changed solution page card partent to el-col components

--- src/views/pages/Solutions/Index.vue
+++ src/views/pages/Solutions/Index.vue
@@ -17,16 +17,25 @@
       </div>
     </div>
     <div class="container cards">
-      <solutions-card
-        v-for="(solutionsCard, index) in solutionsCards"
-        :key="index"
-        :src="solutionsCard.src"
-        :icon-background-color="solutionsCard.iconBackgroundColor"
-        :icon-shadow-color="solutionsCard.iconShadowColor"
-        :title="solutionsCard.title"
-        :description="solutionsCard.description"
-        :read-more-link="solutionsCard.readMoreLink"
-      />
+      <el-row :gutter="24">
+        <el-col
+          v-for="(solutionsCard, index) in solutionsCards"
+          :key="index"
+          :xs="24"
+          :md="12"
+          :lg="6"
+        >
+          <solutions-card
+            :key="index"
+            :src="solutionsCard.src"
+            :icon-background-color="solutionsCard.iconBackgroundColor"
+            :icon-shadow-color="solutionsCard.iconShadowColor"
+            :title="solutionsCard.title"
+            :description="solutionsCard.description"
+            :read-more-link="solutionsCard.readMoreLink"
+          />
+        </el-col>
+      </el-row>
     </div>
   </div>
 </template>
@@ -45,15 +54,16 @@ export default {
     Divider,
     SolutionsCard,
   },
-  computed: {
-    solutionsCards() {
-      return [
+  data() {
+    return {
+      solutionsCards: [
         {
           src: finanacel,
           iconBackgroundColor: '#521FD1',
           iconShadowColor: '4px 7px 17px rgba(82, 31, 209, 0.32)',
           title: this.$t("solutions['financial services']"),
-          description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua..',
+          description:
+            'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua..',
           readMoreLink: '#',
         },
         {
@@ -61,7 +71,8 @@ export default {
           iconBackgroundColor: '#64DDEC',
           iconShadowColor: '4px 7px 17px rgba(100, 221, 236, 0.42)',
           title: this.$t("solutions['media and entertainment']"),
-          description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua..',
+          description:
+            'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua..',
           readMoreLink: '#',
         },
         {
@@ -69,7 +80,8 @@ export default {
           iconBackgroundColor: '#3A71FF',
           iconShadowColor: '4px 7px 17px rgba(58, 113, 255, 0.42)',
           title: this.$t('solutions.healthcare'),
-          description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua..',
+          description:
+            'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua..',
           readMoreLink: '#',
         },
         {
@@ -77,69 +89,70 @@ export default {
           iconBackgroundColor: '#AC46FF',
           iconShadowColor: '4px 7px 17px rgba(172, 70, 255, 0.42)',
           title: this.$t('solutions.education'),
-          description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua..',
+          description:
+            'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua..',
           readMoreLink: '#',
         },
-      ];
-    },
+      ],
+    };
   },
 };
 </script>
 
 <style lang="scss" scoped>
-  .solutions {
-    .header {
-      height: 80vh;
+.solutions {
+  .header {
+    height: 80vh;
 
-      background: url('../../../assets/images/background/header-corner-top.svg') top left no-repeat, url('../../../assets/images/background/header-corner-bottom.svg') bottom right no-repeat, $background-header;
-      color: $text-dark-bg;
+    background: url('../../../assets/images/background/header-corner-top.svg')
+        top left no-repeat,
+      url('../../../assets/images/background/header-corner-bottom.svg') bottom
+        right no-repeat,
+      $background-header;
+    color: $text-dark-bg;
 
-      .divider {
-        margin-bottom: 0.2rem;
-      }
-
-      .container {
-        height: 100%;
-      }
-
-      .content {
-        display: flex;
-        flex-direction: column;
-        align-items: flex-start;
-        justify-content: center;
-
-        height: 100%;
-        width: 100%;
-
-        // TODO: rework this when we have responsive design
-        background-image: url('../../../assets/images/background/solutions-header.svg');
-        background-size: auto 90%;
-        background-position: 90% center;
-        background-repeat: no-repeat;
-      }
-
-      .title {
-        @include typography-title;
-        width: 40%;
-        margin-bottom: 0.16rem;
-      }
-
-      .subtitle {
-        @include typography-subtitle;
-        width: 40%;
-        margin-bottom: 0.48rem;
-      }
-
-      .cta {
-        background: $background-primary;
-      }
+    .divider {
+      margin-bottom: 0.2rem;
     }
-    .cards {
-      display: grid;
-      background: $background-secondary;
-      grid-template-columns: repeat(4, minmax(0, 1fr));
-      padding: 0.6rem .84rem 1.26rem .84rem;
-      column-gap: .24rem;
+
+    .container {
+      height: 100%;
+    }
+
+    .content {
+      display: flex;
+      flex-direction: column;
+      align-items: flex-start;
+      justify-content: center;
+
+      height: 100%;
+      width: 100%;
+
+      // TODO: rework this when we have responsive design
+      background-image: url('../../../assets/images/background/solutions-header.svg');
+      background-size: auto 90%;
+      background-position: 90% center;
+      background-repeat: no-repeat;
+    }
+
+    .title {
+      @include typography-title;
+      width: 40%;
+      margin-bottom: 0.16rem;
+    }
+
+    .subtitle {
+      @include typography-subtitle;
+      width: 40%;
+      margin-bottom: 0.48rem;
+    }
+
+    .cta {
+      background: $background-primary;
     }
   }
+  .cards {
+    padding: 0.6rem 0.84rem 1.26rem 0.84rem;
+  }
+}
 </style>
